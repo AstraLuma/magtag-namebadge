57ea4b253bceac60f35721343606bf745d1235a4
Jamie Bliss <jamie@ivyleav.es>
2022-01-01 16:49:17 -0500

    Use deep sleep

--- code.py
+++ code.py
@@ -6,6 +6,7 @@ import time
 
 import alarm
 from adafruit_magtag.magtag import MagTag
+import board
 
 magtag = MagTag()
 
@@ -19,21 +20,42 @@ except ImportError:
 # Get wifi details and more from a secrets.py file
 try:
     from secrets import name, secrets
-    DISPLAYS = NameTag(magtag=magtag, name=name)
 except ImportError:
     print("WiFi and name secrets are kept in secrets.py, please add them there!")
     raise
 
+DISPLAYS = NameTag(magtag=magtag, name=name)
+
+orig = None
+DISPLAY = 'A'
+
 # Check to see if we are in the first startup
-if not alarm.wake_alarm:
-    DISPLAY = 'A'
+if alarm.wake_alarm:
+    orig = DISPLAY = chr(alarm.sleep_memory[0])
+    if DISPLAY not in 'ABCD':
+        # oh no
+        DISPLAY = 'A'
+        orig = None
+    if isinstance(alarm.wake_alarm, alarm.time.TimeAlarm):
+        # Do nothing, just let the button scan happen
+        pass
+    # TODO: Look for button
+
+for i, b in enumerate(magtag.peripherals.buttons):
+    if not b.value:
+        DISPLAY = 'ABCD'[i]
+        print(f'Button {DISPLAY} pressed')
+        break
+
+if orig != DISPLAY:
     DISPLAYS.render(display=DISPLAY)
 
-while True:
-    for i, b in enumerate(magtag.peripherals.buttons):
-        if not b.value:
-            DISPLAY = 'ABCD'[i]
-            print(f'Button {DISPLAY} pressed')
-            DISPLAYS.render(display=DISPLAY)
-            break
-    time.sleep(0.01)
+alarm.sleep_memory[0] = ord(DISPLAY)
+
+alarm.exit_and_deep_sleep_until_alarms(
+    alarm.time.TimeAlarm(monotonic_time=time.monotonic() + 5),
+    # alarm.pin.PinAlarm(pin=board.D15, value=False, pull=True),
+    # alarm.pin.PinAlarm(pin=board.D14, value=False, pull=True),
+    # alarm.pin.PinAlarm(pin=board.D12, value=False, pull=True),
+    # alarm.pin.PinAlarm(pin=board.D11, value=False, pull=True),
+)
